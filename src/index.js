f3e00791fc4c3499b02290af650ca37da71eedbb
joshpled <joshuapleduc@gmail.com>
2020-09-21 17:16:45 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const task = document.getElementById('new-task-description');
+  const list = document.getElementById('list');
+  const addTask = document.createElement("li");
+
+  function addTaskToListen () {
+  addTask.appendChild(document.createTextNode(task.value));
+  list.appendChild(addTask);
+  }
+
+  document.querySelector('#submit-task').addEventListener("click", function(e) {
+   e.preventDefault();
+  addTaskToListen();
+}, false);
 });
